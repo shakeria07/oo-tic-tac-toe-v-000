e799817b224325e59fe94b5e3b92aef3ad3a53d3
Shakeria Smith <shakerias2@gmail.com>
2018-07-31 21:44:09 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -8,11 +8,11 @@ class TicTacToe
     @board = Array.new(9, " ")
 
     turn until over?
-    
+
     won? ? puts("Congratulations #{winner}!") : puts("Cat's Game!")
-    
+
     puts "Would you like to play again? (Y or N)"
-  
+
     gets.strip.downcase == "y" || gets.strip.downcase == "yes" ? play : puts("Goodbye!")
   end
 
